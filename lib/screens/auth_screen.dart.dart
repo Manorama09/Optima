a0aeea42c97a203981ff8ed9f13484829a862a49
AbhishekSingh2412 <56639395+AbhishekSingh2412@users.noreply.github.com>
2020-05-18 12:40:35 +0530

    Update auth_screen.dart.dart
    Included Welcome Screen in Auth file. Welcome screen navigates to Auth Screen. Line 357 AuthScreen navigates to ProductsOverviewScreen.

--- lib/screens/auth_screen.dart.dart
+++ lib/screens/auth_screen.dart.dart
@@ -1,11 +1,192 @@
 import 'dart:math';
-
+import 'package:optima/main.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/auth.dart';
 import '../models/http_exception.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter_awesome_buttons/flutter_awesome_buttons.dart';
+import 'auth_screen.dart.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'cart_screen.dart';
+import 'package:provider/provider.dart';
+import 'product_detail_screen.dart';
+import 'package:optima/providers/product.dart';
+import 'package:optima/providers/products.dart';
+import 'package:optima/providers/cart.dart';
+import 'package:optima/providers/orders.dart';
+import 'orders_screen.dart';
+import 'products_overview_screen.dart';
+import 'user_products_screen.dart';
+import 'edit_product_screen.dart';
+import 'package:optima/providers/auth.dart';
+
+
+
+
+
+
+
+
+class WelcomeScreen extends StatefulWidget {
+  static const routeName = '/welcome';
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+
+
+
+    animation =
+        ColorTween(begin: Colors.white, end: Colors.white).animate(
+            controller);
+
+    controller.forward();
+
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: animation.value,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+
+            Center(
+              child: Hero(
+                tag: 'new_optima',
+                child: Center(
+                  child: Container(
+                    child: Image.asset('assets/images/optima_logo.png'),
+                    height: 120.0,
+                  ),
+                ),
+              ),
+            ),
+
+
+            SizedBox(
+              width: 250.0,
+              child: FadeAnimatedTextKit(
+                  onTap: () {
+                    print("Tap Event");
+                  },
+                  text: [
+                    "Optimize the experience",
+                    "Hack the COVID Crisis",
+
+
+                  ],
+                  textStyle: TextStyle(
+                      fontFamily: "Montserrat",
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold
+                  ),
+                  textAlign: TextAlign.center,
+                  alignment: AlignmentDirectional.centerStart // or Alignment.topLeft
+              ),
+            ),
+
+
+
+            SizedBox(
+              height: 48.0,
+            ),
+            Center(
+              child: GredientButton(onPressed: (){
+                Navigator.push(context, new MaterialPageRoute(
+                    builder: (context) =>
+                    new AuthScreen()));
+              },
+                radius: 15.0,
+                splashColor: Colors.deepOrangeAccent,
+                colors: [
+                  Colors.orange,
+                  Colors.deepOrange,
+                ],
+                title: "Click here to proceed",
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+
+
+                Hero(
+                  tag: 'ibm',
+                  child: Center(
+                    child: Container(
+                      child: Image.asset('assets/images/ibm.png'),
+                      height: 25.0,
+                    ),
+                  ),
+                ),
+
+
+
+                Hero(
+                  tag: 'nasscom',
+                  child: Center(
+                    child: Container(
+                      child: Image.asset('assets/images/nasscom.png'),
+                      height: 25.0,
+                    ),
+                  ),
+
+                ),
+              ],
+            ),
+
+          ],
+        ),
+
+
+      ),
+
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 enum AuthMode { Signup, Login }
+
 
 class AuthScreen extends StatelessWidget {
   static const routeName = '/auth';
@@ -171,6 +352,10 @@ class _AuthCardState extends State<AuthCard> {
 
     setState(() {
       _isLoading = false;
+      Navigator.push(context, new MaterialPageRoute(
+          builder: (context) =>
+      new ProductsOverviewScreen())
+      );
     });
   }
 
@@ -208,11 +393,12 @@ class _AuthCardState extends State<AuthCard> {
                 TextFormField(
                   decoration: InputDecoration(labelText: 'E-Mail'),
                   keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
+                  validator: (value){
                     if (value.isEmpty || !value.contains('@')) {
                       return 'Invalid email!';
                     }
                   },
+
                   onSaved: (value) {
                     _authData['email'] = value;
                   },
@@ -261,6 +447,7 @@ class _AuthCardState extends State<AuthCard> {
                     color: Theme.of(context).primaryColor,
                     textColor: Theme.of(context).primaryTextTheme.button.color,
                   ),
+
                 FlatButton(
                   child: Text(
                       '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
